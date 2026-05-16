14daa256d8007930615da04938a0792b8f4ad898
ukr23can <tkachenko6591@gmail.com>
2026-05-16 17:32:09 -0300

    add homework 16

--- javascript/lesson15.js
+++ javascript/lesson15.js
@@ -216,3 +216,451 @@ group.run();
 //   checker.render()
 // }
 // runMain();
+// let user = {
+//   name: 'Valentyn',
+//   lastName: 'Tkachenko',
+//   age: 35,
+//   favoriteGames: ['Stalker', 'Resident Eviel', 'The last of us'],
+//   marks: [10, 7, 8, 9, 4, 10, 10],
+//   sayHi: function () {
+//     console.log(`Hello ${this.name} ${this.lastName}`);
+//   },
+//   sayBye: function () {
+//     console.log(`Bye ${this.name} ${this.lastName}`);
+//   },
+//   greeting: function () {
+//     this.sayHi()
+//     this.sayBye()
+//   },
+//   getAvarage: function () {
+//     let s = 0
+//     for (let i = 0; i < this.marks.length; i++) {
+//       s += this.marks[i]
+//     }
+//     return s / this.marks.length
+//   },
+//   getScoreNum: function (searchScore: number) {
+//     return this.marks.reduce((prevCount, score) => score === searchScore ? prevCount + 1 : prevCount, 0)
+//   },
+//   toString: function () {
+//     return `${this.name} ${this.lastName}, Age: ${this.age}, Marks: ${this.marks}`
+//   },
+//   valueOf: function () {
+//     return this.getAvarage()
+//   }
+// };
+// user.sayHi();
+// user.sayBye();
+// // user.greeting()
+// console.log(user.getAvarage().toFixed(1));
+// console.log(user.getScoreNum(10));
+// let s = user.toString()
+// console.log(s);
+// let num = user.valueOf()
+// console.log(num.toFixed(1));
+// console.log(Object.values(user));
+// Task 1 — User Factory
+// function createUser(name: string, age: number) {
+//   return {
+//     name: name,
+//     age: age,
+//     getInfo: function () {
+//       return `Name: ${this.name}, Age: ${this.age}`
+//     },
+//   };
+// };
+// let user1 = createUser('Val', 35);
+// console.log(user1.getInfo());
+// let user2 = createUser('Jonh', 30);
+// console.log(user2.getInfo());
+// Task 2 — Car Factory
+// function createCar(brand: string, speed: number) {
+//   return {
+//     brand: brand,
+//     speed: speed,
+//     showSpeed: function () {
+//       return `The speed: ${this.speed}`
+//     },
+//     changeSpeed: function (newSpeed: number) {
+//       this.speed = newSpeed
+//     },
+//     toString: function () {
+//       return `Brand: ${this.brand}, Speed: ${this.speed}`
+//     },
+//   };
+// };
+// let car1 = createCar('BMW', 250);
+// console.log(car1.showSpeed());
+// console.log(car1.toString());
+// let car2 = createCar('Kia', 230);
+// car2.changeSpeed(300);
+// console.log(car2.showSpeed());
+// console.log(car2.toString());
+// Task 3 — Bank Account
+// function createBankAccount(owner: string, balance: number) {
+//   return {
+//     owner: owner,
+//     balance: balance,
+//     deposit: function (amount: number) {
+//       this.balance += amount
+//     },
+//     withdraw: function (amount: number) {
+//       if (this.balance < amount) {
+//         console.log('Not enough money');
+//         return;
+//       }
+//       this.balance -= amount
+//     },
+//     showBalance: function () {
+//       return console.log(`${this.owner}: ${this.balance}$`);
+//     },
+//   };
+// };
+// let account1 = createBankAccount('Valentyn', 1000);
+// account1.showBalance();
+// account1.deposit(500);
+// account1.showBalance();
+// account1.withdraw(200);
+// account1.showBalance();
+// account1.withdraw(800);
+// account1.showBalance();
+// account1.withdraw(800);
+// account1.showBalance();
+// Task 4 — Counter
+// function createCounter() {
+//   let count = 0
+//   return {
+//     increment() {
+//       count++
+//     },
+//     decrement() {
+//       count--
+//     },
+//     reset() {
+//       count = 0
+//     },
+//     show() {
+//       console.log(count);
+//     },
+//   };
+// };
+// let counter1 = createCounter()
+// counter1.increment()
+// counter1.increment()
+// counter1.show()
+// Task 5 — Playlist
+// type PlaylistType = {
+//   name: string;
+//   songs: string[];
+//   addSongs(song: string): void;
+//   removeSong(song: string): void;
+//   showSongs(): void;
+// }
+// function createPlaylist(name: string) {
+//   return {
+//     name: name,
+//     songs: [],
+//     addSongs: function (song: string) {
+//       this.songs.push(song);
+//     },
+//     removeSong: function (song: string) {
+//       this.songs = this.songs.filter(item => item !== song)
+//     },
+//     showSongs() {
+//       console.log(`Playlist: ${this.name}`);
+//       this.songs.forEach((song, index) => {
+//         console.log(`${index + 1}. ${song}`);
+//       })
+//     }
+//   }
+// }
+// const playlist = createPlaylist('My favorite');
+// console.log(playlist);
+// playlist.addSongs('Linking Park')
+// playlist.addSongs('Emenim')
+// playlist.addSongs('The Offspring')
+// playlist.showSongs()
+// playlist.removeSong('The Offspring')
+// playlist.showSongs()
+// ========================================================
+// task 1
+// type PlayerType = {
+//   name: string,
+//   score: number,
+//   showScore: () => void,
+//   addScore: (points: number) => void,
+// }
+// function Player(this: PlayerType, name: string, score: number) {
+//   this.name = name,
+//     this.score = score,
+//     this.showScore = function () {
+//       console.log(`Score: ${this.score}`);
+//     },
+//     this.addScore = function (points: number) {
+//       this.score += points
+//     }
+// }
+// const player1 = new Player('Val', 100)
+// player1.showScore()
+// player1.addScore(50)
+// player1.showScore()
+// Task 2 — Bank Account
+// type BankAccountType = {
+//   owner: string,
+//   balance: number,
+//   deposit: (amount: number) => void,
+//   withdraw: (amount: number) => void,
+//   showBalance: () => void
+// }
+// function BankAccount(this: BankAccountType, owner: string, balance: number) {
+//   this.owner = owner,
+//     this.balance = balance,
+//     this.deposit = function (amount: number) {
+//       this.balance += amount
+//     },
+//     this.withdraw = function (amount: number) {
+//       if (amount > this.balance) {
+//         throw new Error("Not enough money");
+//       }
+//       this.balance -= amount
+//     },
+//     this.showBalance = function () {
+//       console.log(`${this.owner} balance: ${this.balance}`);
+//     }
+// }
+// const acc1 = new BankAccount('Val', 1000)
+// console.log(acc1);
+// acc1.deposit(500);
+// acc1.showBalance();
+// acc1.withdraw(800);
+// acc1.showBalance();
+// task 3 Playlist
+// type PlaylistType = {
+//   name: string,
+//   songs: string[],
+//   addSong: (song: string) => void,
+//   removeSong: (song: string) => void,
+//   showSongs: () => void,
+// }
+// function Playlist(this: PlaylistType, name: string) {
+//   this.name = name,
+//     this.songs = [],
+//     this.addSong = function (song: string) {
+//       this.songs.push(song)
+//     },
+//     this.removeSong = function (song: string) {
+//       this.songs = this.songs.filter(item => item !== song)
+//     },
+//     this.showSongs = function () {
+//       this.songs.forEach((song, index) => console.log(`${index + 1}. ${song} `));
+//     }
+// }
+// const playlist = new Playlist('My playlist')
+// console.log(playlist);
+// playlist.addSong('Numb');
+// playlist.addSong('My heart');
+// playlist.addSong('In the end');
+// playlist.removeSong('My heart')
+// playlist.showSongs();
+// task - playlist prototype
+// type PlaylistType = {
+//   name: string,
+//   songs: string[],
+// }
+// function Playlist(this: PlaylistType, name: string) {
+//   this.name = name,
+//     this.songs = []
+// }
+// Playlist.prototype.addSong = function (song: string) {
+//   this.songs.push(song);
+// }
+// Playlist.prototype.removeSong = function (song: string) {
+//   this.songs = this.songs.filter(item => item !== song)
+// }
+// Playlist.prototype.showSongs = function () {
+//   this.songs.forEach((song, index) => console.log(`${index + 1}. ${song} `));
+// }
+// const playlist = new Playlist('My playlist')
+// console.log(playlist);
+// playlist.addSong('My love')
+// playlist.addSong('My friend')
+// playlist.addSong('At the work')
+// playlist.addSong('Mother')
+// playlist.removeSong('At the work')
+// playlist.showSongs()
+// ==============================================================
+// ======================== classes ========================
+// class User {
+//   name: string;
+//   age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   sayHello() {
+//     console.log(`Hello, I'm ${this.name}`);
+//   }
+// }
+// const user1 = new User("Val", 33);
+// user1.sayHello();
+// ======================
+// class BankAccount {
+//   owner: string;
+//   balance: number;
+//   constructor(owner: string, balance: number) {
+//     this.owner = owner;
+//     this.balance = balance;
+//   };
+//   deposit(amount: number) {
+//     this.balance += amount
+//   };
+//   withdraw(amount: number) {
+//     if (amount > this.balance) {
+//       throw new Error("Not enough money");
+//     }
+//     this.balance -= amount
+//   };
+//   showBalance() {
+//     console.log(`${this.owner} balance: ${this.balance}`);
+//   }
+// }
+// const acc1 = new BankAccount('Val', 1000)
+// console.log(acc1);
+// acc1.deposit(500);
+// acc1.withdraw(800);
+// acc1.withdraw(700);
+// acc1.showBalance();
+// ====================================
+// Task — Todo List (Class OOP)
+// class Todolist {
+//   title: string;
+//   tasks: string[];
+//   constructor(title: string) {
+//     this.title = title;
+//     this.tasks = []
+//   };
+//   addTask(task: string) {
+//     this.tasks.push(task)
+//   };
+//   removeTask(task: string) {
+//     // this.tasks = this.tasks.filter(item => item !== task)
+//     const index = this.tasks.indexOf(task)
+//     if (index !== -1)
+//       this.tasks.splice(index, 1)
+//   };
+//   showTask() {
+//     this.tasks.forEach((task, index) => console.log(`${index + 1}. ${task}`));
+//   };
+//   clearTask() {
+//     this.tasks.length = 0
+//   }
+// }
+// const task1 = new Todolist('My Day')
+// task1.addTask('Learn JS');
+// task1.addTask('Show a lesson JS')
+// task1.addTask('Learn English');
+// task1.addTask('Cook Dinner');
+// task1.removeTask('Cook Dinner');
+// // // task1.clearTask()
+// task1.showTask()
+// console.log(task1);
+// ==============================================================
+// let obj1 = {
+//   myVar: 77,
+//   method1: function () {
+//     let func = function () {
+//       console.log(this);
+//       document.write(this.myVar);
+//     }
+//     return func
+//   },
+// }
+// let f = obj1.method1()
+// f()
+// let obj1 = {
+//   names: ['Ivan', 'Petro', 'Olga', 'Hanna'],
+//   poritions: ['Driver', 'Manager', 'Programmer', 'Director'],
+//   showNames: function () {
+//     this.names.forEach((name, index) => {
+//       document.write(`
+//             ${name} ${this.poritions && this.poritions[index]} <br>
+//         `);
+//     });
+//   },
+// }
+// obj1.showNames()
+// ==============================================================
+// =============== call / apply =================
+// let obj1 = {
+//   prop1: 11,
+//   prop2: 22,
+//   showProp: function () {
+//     document.write(this.prop1);
+//   },
+//   getSum: function (val1: number, val2: number) {
+//     return this.prop1 + val1 + val2
+//   }
+// }
+// // obj1.showProp()
+// let obj2 = {
+//   prop1: 21,
+//   prop2: 10,
+// }
+// // obj1.showProp.call(obj2)
+// let s = obj1.getSum.call(obj2, 10, 7)
+// document.write(s);
+// const user = {
+//   name: "Val",
+//   age: 35,
+// };
+// const user2 = {
+//   name: 'Stas',
+//   age: 30,
+// }
+// function showUser(city: string, country: string) {
+//   console.log(`Name: ${this.name}, Age: ${this.age}, from: ${city}, ${country}`);
+// }
+// showUser.call(user, 'Mariupol', 'Ukraine');
+// showUser.apply(user2, ['Kyiv', 'Ukraine']);
+// ==============================================================
+// ================== bind ======================
+// const user = {
+//   name: "Val"
+// };
+// function showUser() {
+//   console.log(this.name);
+// }
+// const newFunc = showUser.bind(user);
+// newFunc();
+// const user = {
+//   name: "Val",
+//   show() {
+//     console.log(this.name);
+//   }
+// };
+// setTimeout(user.show.bind(user), 2000);
+// // Task 1
+// const user = {
+//   name: "Alex"
+// };
+// function sayHello(this: typeof user) {
+//   console.log(`Hello ${this.name}`);
+// }
+// sayHello.call(user)
+// // Task 2
+// const player = {
+//   name: "Mike"
+// };
+// function showInfo(this: typeof player, age: number) {
+//   console.log(this.name, age);
+// }
+// const boundShowInfo = showInfo.bind(player)
+// boundShowInfo(30)
+// // task 3
+// const data = {
+//   title: "Result:"
+// };
+// function sum(this: typeof data, a: number, b: number) {
+//   console.log(this.title, a + b);
+// }
+// sum.apply(data, [2, 3])
---
+++ src/lesson16.ts
@@ -0,0 +1,328 @@
+//  Task 1. Клас із полями #weight і #items. Дозвольте додавати предмети, якщо вага не перевищує 10 кг.
+document.write(`<h2>Task 2</h2>`);
+type ItemType = {
+  name: string;
+  weight: number;
+}
+
+class Backpack {
+  #weight: number = 0;
+  #items: ItemType[] = [];
+  constructor() {
+    this.#weight = 0;
+    this.#items = [];
+  }
+  addItem(name: string, itemWeight: number): void {
+    if (this.#weight + itemWeight <= 10) {
+      const newItem: ItemType = {
+        name,
+        weight: itemWeight,
+      }
+      this.#items.push(newItem);
+      this.#weight += itemWeight;
+      console.log(`✅ Додано: ${name} (${itemWeight}кг)`);
+    } else
+      console.error(`❌ Неможливо додати "${name}". Перевищення ліміту ваги!`);
+  }
+  get showWeight(): number {
+    return this.#weight
+  }
+  get showItems(): ItemType[] {
+    return [...this.#items]
+  }
+  toString() {
+    return this.#items.map(item => `${item.name} (${item.weight}kg)`).join(', ')
+  }
+}
+const myBackpack = new Backpack()
+myBackpack.addItem('Laptop', 3);
+myBackpack.addItem('Books', 4);
+myBackpack.addItem('Bottle of water', 1);
+console.log(`Загальна вага: ${myBackpack.showWeight}`);
+console.log(myBackpack.showItems);
+console.log(myBackpack.toString());
+
+
+
+// Task 3. Клас із полями #ingredients і #orders. Замовлення можливі, якщо є інгредієнти.
+document.write(`<h2>Task 3</h2>`);
+type IngredientType = {
+  name: string;
+  amount: number;
+}
+class Pizzeria {
+  #ingredients: IngredientType[];
+  #orders: string[];
+
+  constructor() {
+    this.#ingredients = [];
+    this.#orders = [];
+  }
+  addIngredients(name: string, amount: number) {
+    if (name && amount > 0) {
+      const existing = this.#ingredients.find(item => item.name.toLowerCase() === name.toLowerCase());
+
+      if (existing) {
+        existing.amount += amount;
+      } else
+        this.#ingredients.push({ name: name.toLowerCase(), amount });
+    }
+    document.write(`<p>Додано інгредієнт: ${name}, (${amount}gr)</p><hr>`);
+  }
+  createOrder(dishName: string, needsIngredients: string, requiredAmount: number = 1): void {
+    const ingredient = this.#ingredients.find(item => item.name.toLowerCase() === needsIngredients.toLowerCase());
+    if (!ingredient || ingredient.amount < requiredAmount) {
+      document.write(`<p>Неможливо приготувати "${dishName}".Недостатньо інгредієнта: ${needsIngredients}</p><hr>`);
+      return;
+    }
+    ingredient.amount -= requiredAmount;
+    this.#orders.push(dishName);
+    document.write(`<p>Замовлення прийнято: ${dishName}.Використано ${requiredAmount}г ${needsIngredients}</p><hr>`);
+  }
+  get ordersHistory(): string[] {
+    return [...this.#orders];
+  }
+  toString() {
+    return this.#ingredients.map(item => (`${item.name}: ${item.amount}г`)).join(' | ') || "Склад порожній";
+  }
+}
+const myPizza = new Pizzeria()
+// ==============
+myPizza.addIngredients('Dough', 2000);
+myPizza.addIngredients('Tomato', 500);
+myPizza.addIngredients('Cheese', 300);
+// ==============
+myPizza.createOrder('Papperoni', 'cheese', 200)
+myPizza.createOrder('Margarita', 'tomato', 150)
+// ==============
+document.write(`<p>${myPizza.ordersHistory}</p>`);
+document.write(myPizza.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =============================================================
+// class User {
+//   #age = 0; // Приватне поле
+
+//   constructor(age: number) {
+//     this.#age = age
+//   }
+
+//   // Геттер: повертає значення
+//   get age() {
+//     return this.#age;
+//   }
+//   // Сеттер: встановлює значення з перевіркою
+//   set age(value: number) {
+//     if (value < 0 || value > 120) {
+//       console.error("Некоректний вік!");
+//       return;
+//     }
+//     this.#age = value;
+//   }
+//   showAge() {
+//     console.log(`${ this.#age }`);
+//   }
+// }
+
+// const user = new User(25);
+// user.showAge()
+// user.age = 25; // Викликається set age(25)
+// console.log(user.age); // Викликається get age() -> 25
+
+
+// =============================================================
+
+
+
+
+
+
+
+
+
+
+// ======================================================
+// class Password {
+//   #password: string;
+
+//   constructor(password: number) {
+//     this.#password = password;
+//   }
+
+//   setPassword(newPassword: string): void {
+//     if (newPassword.length >= 6) {
+//       this.#password = newPassword;
+//       console.log("✅ Пароль успішно оновлено.");
+//     } else
+//       console.error("❌ Помилка: Пароль має містити мінімум 6 символів.");
+//   }
+//   checkPassword(candidate: string): boolean {
+//     return this.#password === candidate
+//   }
+// }
+
+
+// class BankAccount {
+//   public name: string;
+//   #money: number;
+
+//   constructor(name: string, money: number) {
+//     this.#money = money;
+//     this.name = name;
+//   }
+//   deposit(amount: number) {
+//     if (amount > 0) {
+//       this.#money += amount
+//     }
+//   }
+//   withdraw(amount: number) {
+//     if (amount <= this.#money) {
+//       this.#money -= amount
+//     }
+//   }
+//   get getMoney() {
+//     return this.#money
+//   }
+// }
+
+// const user1 = new BankAccount('Val', 1000);
+// user1.deposit(300)
+// user1.withdraw(500)
+// console.log(user1.getMoney);
+
+// const user3 = new Password('1234567')
+// console.log(user3.checkPassword('123456'));
+// ======================
+// task 1
+// class Temperature {
+//   #celsius: number;
+//   constructor(celsius: number) {
+//     this.#celsius = celsius
+//   }
+//   set setTemperature(value: number) {
+//     if (value < -273.15) {
+//       console.log('The temperature is too low');
+//       return
+//     }
+//     this.#celsius = value
+//   }
+//   get getTemperature() {
+//     return this.#celsius
+//   }
+//   toFahrenheit() {
+//     return this.#celsius * (9 / 5) + 32
+//   }
+// }
+// const temp = new Temperature(100)
+// temp.setTemperature = -270
+// console.log(temp.getTemperature);
+// console.log(temp.toFahrenheit());
+
+// ======================
+// task 2
+// class Player {
+//   #name: string;
+//   #health: number;
+
+//   constructor(name: string, health: number) {
+//     this.#health = Math.min(Math.max(health, 0), 100)
+//     this.#name = name;
+//   }
+//   takeDamage(value: number) {
+//     this.#health = Math.max(this.#health - value, 0);
+//     console.log(`${ this.#name } отримав ${ value } шкоди.Поточне здоров'я: ${this.#health}`);
+//   }
+//   health(value: number) {
+//     this.#health = Math.min(this.#health + value, 100)
+//     console.log(`${this.#name} полікувався на ${value}. Поточне здоров'я: ${this.#health}`);
+//   }
+//   get totalHealth() {
+//     return `Player: ${this.#name}, Health: ${this.#health}`;
+//   }
+// }
+// const player1 = new Player('Val', 50)
+// player1.takeDamage(60)
+// player1.takeDamage(60)
+// player1.health(200)
+// player1.totalHealth
+
+// ======================
+// task 3 ShoppingCart
+// class ShoppingCart {
+//   #items: string[];
+//   constructor() {
+//     this.#items = [];
+//   }
+//   addItem(item: string): void {
+//     if (item.length > 0) {
+//       this.#items.push(item)
+//       console.log(`✅ Додано: ${item}`);
+//     } else
+//       console.log('Item incorrect!');
+//   }
+//   removeItem(item: string): void {
+//     this.#items = this.#items.filter(i => i !== item)
+//   }
+//   showItems() {
+//     return this.#items
+//   }
+//   get totalItems() {
+//     return this.#items.length
+//   }
+// }
+// const cart1 = new ShoppingCart();
+// cart1.addItem('laptop');
+// cart1.addItem('Pan');
+
+// // cart1.removeItem('Pan')
+// console.log(cart1.showItems());
+// console.log(cart1.totalItems);
+// console.log(cart1);
+
+
+// task 7 UserAccount
+// class UserAccount {
+//   #email: string;
+//   #password: string;
+
+//   constructor(email: string, password: string) {
+//     this.#email = email;
+//     this.#password = password;
+//   }
+//   changeEmail(newEmail: string): void {
+//     if (newEmail.includes('@')) {
+//       this.#email = newEmail
+//       console.log("✅ Email успішно змінено.");
+//     } else
+//       console.error("❌ Помилка: Email має містити символ '@'.");
+//   }
+//   changePassword(newPassword: string): void {
+//     if (newPassword.length > 8) {
+//       this.#password = newPassword
+//       console.log("✅ Пароль оновлено.");
+//     } else
+//       console.error("❌ Помилка: Пароль має бути не менше 8 символів.");
+//   }
+//   get email(): string {
+//     return this.#email
+//   }
+// }
+// const user1 = new UserAccount('test@example.com', 'secure123')
+
+// user1.changeEmail('wrong-email.com')
+// user1.changePassword('123');
+// user1.changeEmail('new-user@gmail.com')
+// user1.changePassword('my-new-long-password')
+// console.log(user1.email);
